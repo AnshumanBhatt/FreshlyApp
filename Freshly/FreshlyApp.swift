43a0fbcd83c2084dec4ee5f2c8e1689d483920c3
AnshumanBhatt <anshumanbhatt1212@gmail.com>
2024-11-21 20:27:08 +0530

    Initial Commit

---
+++ Freshly/FreshlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreshlyApp.swift
+//  Freshly
+//
+//  Created by Anshuman Bhatt on 21/11/24.
+//
+
+import SwiftUI
+
+@main
+struct FreshlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
